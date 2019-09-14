1c70946efacead48d042c55fb77e3619d829ea98
Nim Wijetunga <nimwijetunga@Nims-MacBook-Pro.local>
2019-09-14 13:44:49 -0400

    replace dupes

--- data/get_postal_data.py
+++ data/get_postal_data.py
@@ -2,17 +2,14 @@ import pandas as pd
 
 def get_sri_data():
   postal_beg = ['K','L','M','N', 'P']
-  postal_codes = []
-  places = []
+  postal_codes = set()
   for prefix in postal_beg:
     df = pd.read_csv('%s_Postal.csv' % prefix)
     cur_postal_code = ''
     for i, row in df.iterrows():
-      if row['FSA'] != cur_postal_code:
-        postal_codes.append(row['FSA'])
-        places.append(row['Place Name'])
-      cur_postal_code = row['FSA']
-  result_df = pd.DataFrame({'postal_codes': postal_codes, 'places': places})
+      postal_codes.add(row['FSA'])
+  postal_codes = list(postal_codes)
+  result_df = pd.DataFrame({'postal_codes': postal_codes})
   result_df.reset_index().to_csv('postal_code_data.csv', index=False)
 
 if __name__ == '__main__':
