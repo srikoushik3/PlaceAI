2e0f454a0435f8fc9f5f830a2c9459e4b61c5eec
Nim Wijetunga <nimwijetunga@Nims-MacBook-Pro.local>
2019-09-15 00:26:12 -0400

    Add stuff

--- data/uploadToFireStore.js
+++ data/uploadToFireStore.js
@@ -19,15 +19,15 @@ fs.createReadStream('./geometry_information.csv')
     data.push(row)
   })
   .on('end', () => {
-    console.log(data);
-    data.forEach(e => {
+    data.forEach(async (row) => {
       data = {
         place: row['place'],
-        lat: row['lat'],
-        lng: row['lng'],
-        radius: row['radius'],
-        width: row['width'],
-        height: row['height']
+        lat: parseFloat(row['lat']),
+        lng: parseFloat(row['lng']),
+        radius: parseFloat(row['radius']),
+        width: parseFloat(row['width']),
+        height: parseFloat(row['height']),
+        postal_code: row['postal_code']
       }
       let setDoc = await db.collection('PostalCodes').doc(row['postal_code']).set(data);
     });
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});  
+var request = require('request');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
@@ -19,20 +20,93 @@ async function get_data(postal_code){
   }
 }
 
+function makeRequest(url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+// http://us-central1-hackthenorth.cloudfunctions.net/getOptimalPostalCodes?ageRangeLow=${data.ageRange[0]}&ageRangeHigh=${data.ageRange[1]}&incomeRangeLow=${data.incomeRange[0]}&incomeRangeHigh=${data.incomeRange[1]}&familySize=${data.familySize}
 exports.getOptimalPostalCodes = functions.https.onRequest(async (req, res) => {
-  postal_codes = ['KOA', 'K0B', 'L5M', 'L8L', 'L8M', 'L9T', 'K1C', 'M4B', 'M4C', 'M4G']
   postal_code_data = []
   let count = 1
+  let ageRangeHigh = parseInt(req.query.ageRangeHigh)
+  let ageRangeLow = parseInt(req.query.ageRangeLow)
+  let incomeRangeLow = parseInt(req.query.incomeRangeLow)
+  let incomeRangeHigh = parseInt(req.query.incomeRangeHigh)
+  let avgAge = parseInt(ageRangeHigh + ageRangeLow)/2.0
+  let avgIncome = parseInt(incomeRangeLow + incomeRangeHigh)/2.0
+  let url = `http://evening-journey-71031.herokuapp.com/getPredictions?age=${avgAge}&income=${avgIncome}`
+  console.log(url)
+  let postal_codes = await makeRequest(url)
+  postal_codes = JSON.parse(postal_codes)
+  postal_codes = postal_codes['postal_codes']
+  console.log(postal_codes)
   for(code of postal_codes){
     d = await get_data(code)
     if(!d){
       continue;
     }
     d['strength'] = count
-    console.log(d)
     postal_code_data.push(d)
     count++
   }
   res.set('Access-Control-Allow-Origin', '*')
   res.send(JSON.stringify({'postal_code_data': postal_code_data}))
+});
+
+exports.getCompetitorData = functions.https.onRequest(async (req, res) => {
+  fake_data = {heatMapData: {
+    positions: [{
+      lat: 43.8890,
+      lng: -79.6441,
+      weight: 1
+    }, {
+      lat: 43.8990,
+      lng: -79.6241,
+      weight: 0.3
+    }, {
+      lat: 43.9190,
+      lng: -79.6341,
+      weight: 1
+    }, {
+      lat: 43.9290,
+      lng: -79.6541,
+      weight: 0.4
+    }, {
+      lat: 43.8790,
+      lng: -79.6441,
+      weight: 0.2
+    }, {
+      lat: 43.8190,
+      lng: -79.6341,
+      weight: 0.4
+    }, {
+      lat: 43.9090,
+      lng: -79.6241,
+      weight: 0.9
+    }]
+  },competitorData: {
+    positions: [{
+      lat: 43.8890,
+      lng: -79.6441,
+      name: "C"
+    }, {
+      lat: 43.8990,
+      lng: -79.6241,
+      name: "B"
+    }, {
+      lat: 43.9190,
+      lng: -79.6341,
+      name: "A" }
+    ]
+  }
+}
+    res.send(JSON.stringify(fake_data))
 });
