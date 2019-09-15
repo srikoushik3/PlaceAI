9f562412f5017893a9824a2c7a2634f46ec20538
Nim Wijetunga <nimwijetunga@Nims-MacBook-Pro.local>
2019-09-14 21:42:22 -0400

    change

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
+const cors = require('cors')({origin: true});  
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
@@ -19,16 +20,19 @@ async function get_data(postal_code){
 }
 
 exports.getOptimalPostalCodes = functions.https.onRequest(async (req, res) => {
-  if (req.method != "POST") {
-    respond.status(400).send("Invalid Request");
-    return;
-  }
   postal_codes = ['KOA', 'K0B', 'L5M', 'L8L', 'L8M', 'L9T', 'K1C', 'M4B', 'M4C', 'M4G']
   postal_code_data = []
+  let count = 1
   for(code of postal_codes){
     d = await get_data(code)
+    if(!d){
+      continue;
+    }
+    d['strength'] = count
     console.log(d)
     postal_code_data.push(d)
+    count++
   }
+  res.set('Access-Control-Allow-Origin', '*')
   res.send(JSON.stringify({'postal_code_data': postal_code_data}))
 });
